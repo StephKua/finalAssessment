3394b3a5137e50473497eacc2fa1cd61f69faea0
Skkz <Me@Skkzs-MacBook-Air.local>
2016-08-25 12:37:57 +0800

    status

---
+++ FinalAssessment/Status.swift
@@ -0,0 +1,16 @@
+//
+//  Status.swift
+//  FinalAssessment
+//
+//  Created by Skkz on 25/08/2016.
+//  Copyright © 2016 Skkz. All rights reserved.
+//
+
+import Foundation
+
+class Status {
+    
+    var statusInfo: String!
+    var statusID: String!!
+    
+}
